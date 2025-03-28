b510289972d4b95d2013b352d88da6b4af1a443f
Ivan-Yakovl <ivan-yakovl@yandex.ru>
2025-03-28 12:11:52 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
-// Функция для выполнения LU-разложения
-void LU(vector<vector<double>>& A, vector<vector<double>>& L, vector<vector<double>>& U, int n) {
+
+void LU(vector<vector<double>> A, vector<vector<double>>& L,
+    vector<vector<double>>& U, int n)
+{
     U = A;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
+    for (int i = 0; i < n; i++)
+        for (int j = i; j < n; j++)
             L[j][i] = U[j][i] / U[i][i];
-        }
 
-        for (int k = i + 1; k < n; k++) {
-            for (int j = i; j < n; j++) {
-                U[k][j] = U[k][j] - L[k][i] * U[i][j];
-            }
-        }
+    for (int k = 1; k < n; k++)
+    {
+        for (int i = k - 1; i < n; i++)
+            for (int j = i; j < n; j++)
+                L[j][i] = U[j][i] / U[i][i];
+
+        for (int i = k; i < n; i++)
+            for (int j = k - 1; j < n; j++)
+                U[i][j] = U[i][j] - L[i][k - 1] * U[k - 1][j];
     }
 }
 
-// Функция для решения системы Ly = b
-void solveLy(const vector<vector<double>>& L, vector<double>& y, const vector<double>& b, int n) {
+
+void matrixMultiply(vector<vector<double>> A, vector<vector<double>> B,
+    vector<vector<double>>& R, int n)
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int k = 0; k < n; k++)
+                R[i][j] += A[i][k] * B[k][j];
+}
+
+
+void solveLy(vector<vector<double>> L, vector<double> b, vector<double>& y, int n)
+{
     for (int i = 0; i < n; i++) {
         y[i] = b[i];
         for (int j = 0; j < i; j++) {
@@ -31,8 +48,9 @@ void solveLy(const vector<vector<double>>& L, vector<double>& y, const vector<do
     }
 }
 
-// Функция для решения системы Ux = y
-void solveUx(const vector<vector<double>>& U, vector<double>& x, const vector<double>& y, int n) {
+
+void solveUx(vector<vector<double>> U, vector<double> y, vector<double>& x, int n)
+{
     for (int i = n - 1; i >= 0; i--) {
         x[i] = y[i];
         for (int j = i + 1; j < n; j++) {
@@ -43,25 +61,36 @@ void solveUx(const vector<vector<double>>& U, vector<double>& x, const vector<do
 }
 
 
-void show(const vector<vector<double>>& A, int n) {
+void showMatrix(vector<vector<double>> A, int n)
+{
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cout << "\t" << A[i][j] << "\t";
+            cout << setw(10) << A[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-int main() {
+void showVector(vector<double> v, int n)
+{
+    for (int i = 0; i < n; i++) {
+        cout << "[" << i << "] = " << v[i] << endl;
+    }
+}
+
+int main()
+{
     int n;
-    cout << "Введите размер матрицы n: ";
+    cout << "Enter matrix size n: ";
     cin >> n;
 
-   
-    vector<vector<double>> A(n, vector<double>(n)), L(n, vector<double>(n)), U(n, vector<double>(n));
+    vector<vector<double>> A(n, vector<double>(n)),
+        L(n, vector<double>(n, 0)),
+        U(n, vector<double>(n, 0)),
+        R(n, vector<double>(n, 0));
     vector<double> b(n), y(n), x(n);
 
-    cout << "Введите элементы матрицы A:" << endl;
+    cout << "\nEnter matrix A elements:" << endl;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cout << "A[" << i << "][" << j << "] = ";
@@ -69,35 +98,41 @@ int main() {
         }
     }
 
-    
-    cout << "Введите элементы вектора b:" << endl;
+    cout << "\nEnter vector b elements:" << endl;
     for (int i = 0; i < n; i++) {
         cout << "b[" << i << "] = ";
         cin >> b[i];
     }
 
-    // LU-разложение
+  
     LU(A, L, U, n);
 
-    cout << "Матрица A:" << endl;
-    show(A, n);
+    cout << "\nOriginal matrix A:" << endl;
+    showMatrix(A, n);
 
-    cout << "Матрица L:" << endl;
-    show(L, n);
+    cout << "\nLower triangular matrix L:" << endl;
+    showMatrix(L, n);
 
-    cout << "Матрица U:" << endl;
-    show(U, n);
+    cout << "\nUpper triangular matrix U:" << endl;
+    showMatrix(U, n);
 
-    // Решение системы Ly = b
-    solveLy(L, y, b, n);
+ 
+    matrixMultiply(L, U, R, n);
+    cout << "\nVerification: L*U =" << endl;
+    showMatrix(R, n);
 
-    // Решение системы Ux = y
-    solveUx(U, x, y, n);
+  
+    cout << "\nSolving the system Ax = b:" << endl;
 
-    cout << "Решение вектора x:" << endl;
-    for (int i = 0; i < n; i++) {
-        cout << "x[" << i << "] = " << x[i] << endl;
-    }
+   
+    solveLy(L, b, y, n);
+    cout << "\nIntermediate vector y (from Ly = b):" << endl;
+    showVector(y, n);
+
+   
+    solveUx(U, y, x, n);
+    cout << "\nSolution vector x (from Ux = y):" << endl;
+    showVector(x, n);
 
     return 0;
 }
